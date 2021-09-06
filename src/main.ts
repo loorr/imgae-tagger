54e668c0e2af12958cb8b221c217500a9dc38355
jove <zjianfa@foxmail.com>
2021-09-06 20:39:39 +0800

    add:引入 element-plus

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/index'
-import store from './store/index'
+import router from '../src/router/index'
+import store from '../src/store/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 createApp(App)
     .use(router)
+    .use(ElementPlus)
     .use(store)
     .mount('#app')
